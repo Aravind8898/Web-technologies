1e0b0e35e9bf56eaa41bad8127388ff34db9adf8
Aravind <aravindhpenumetcha@gmail.com>
2021-06-06 20:09:06 +0100

    .

--- job.js
+++ job.js
@@ -2,24 +2,24 @@ fetch('Jobs.json')
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
+    .then(function (data){
         appendData(data);
-        function appendData(data) {
-            var main = document.getElementById("demo");
-           
-            for (var i = 0; i < 1; i++)
+        function appendData(data)
+        {
+            var main = document.getElementById("demo")
+            for (var i = 0; i < data.length; i++)
             {
                 var div = document.createElement("div");
-                div.innerHTML = 'JobTitle: ' + data[i].JobTitle;
-                main.appendChild(div);
-                var div = document.createElement("div");
-                div.innerHTML = 'Location: ' + data[i].Location;
-                main.appendChild(div);
-                var div = document.createElement("div");
-                div.innerHTML = 'Company: ' + data[i].company;
-                main.appendChild(div);
-
+                div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company;
+                main.appendChild(div);     
             }
+           
         }
-    
-})
+       
+    })
+
+
+
+
+
+
