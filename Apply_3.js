b9d77e4a8c62b2590a5c67a73b7f37004bae5f07
Aravind <aravindhpenumetcha@gmail.com>
2021-06-11 05:53:53 +0100

    .

--- Apply_3.js
+++ Apply_3.js
@@ -1,4 +1,4 @@
- function regis() //using function to call the validations below:
+ function regis()  // function for validation this function is called at submit botton
 {   
     var name = document.getElementById("fname").value;
     var name = document.getElementById("lname").value;
@@ -8,16 +8,17 @@
     var kill = document.getElementById("skills").value;
     var noo = document.getElementById("No").value;
 
-    //All letters and numbers are filter using regex
+    
     var letters = /^[A-Za-z]+$/;
     var emails = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var numbers = /^([0-9])+$/;
+     
   
     if (name == "")
     {
         alert("Mandatory Field: You should not leave it blank!!");
     }
-    else if (!letters.test(name)) // Here test method is used. // Alphabets check
+    else if (!letters.test(name)) 
     {
         alert("please enter the Alphabets")
     }
@@ -25,16 +26,16 @@
     {
         alert("Please enter the Email Address")
     }
-    else if (!emails.test(emil)) //test method is used //Email check
+    else if (!emails.test(emil)) 
     {
         alert("Please enter the vaild Emailid")
     }
-    else if (!numbers.test(expe)) //test method to check only numeric values.
+    else if (!numbers.test(expe)) 
     {
         alert("please enter only numeric values")
     } 
     
-    else if (jo == "")     //This feild should not leave blank
+    else if (jo == "")     
     {
         alert("You should not leave it blank!!")
 
@@ -53,9 +54,10 @@
     }
     else if (!numbers.test(noo))
     {
-        alert("Enter numeric values")
+        alert("Enter numeric values with 10 digits")
     }
-    else                             // if the above conditions pass then successfully application will submitted
+    
+    else                             
     {
         alert("Thank you for applying for this Job. We will reach out to you soon!!") 
               
--- job.js
+++ job.js
@@ -1,12 +1,14 @@
-fetch('Jobs.json')       //using fetch API to get the data from JSON file
-    .then(function (response) {   //return the data
+fetch('Jobs.json')       //used fetch function here
+    .then(function (response) {   //got the response from the JSON
         return response.json();
     })
-    .then(function (data){   //actual JSON data here
-        appendData(data);
-        function appendData(data) //used append function here
+    .then(function (data)
+    {
+        appendData(data);  //Actuall JSON data into data
+        
+        function appendData(data) //used append function
         {
-            var main = document.getElementById("demo")
+            var main = document.getElementById("demo")   //id demo is called in index.html 
             var one = document.getElementById("second")
             var two = document.getElementById("three")
             var four = document.getElementById("four")
@@ -16,57 +18,56 @@ fetch('Jobs.json')       //using fetch API to get the data from JSON file
             var eight = document.getElementById("eight")
             var nine = document.getElementById("nine")
             var ten = document.getElementById("ten")
-           // for (i = 0; i < data.length; i++)
-            //{
+           
                 i= 1
                 var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
+                div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
                 main.appendChild(div);
-          //  }
-                i = 2
-                 var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company +  "<br>" + 'JobType: ' + data[i].JobType;
-            one.appendChild(div);
+         
+               i = 2
+               var div = document.createElement("div");
+               div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+               one.appendChild(div);
 
-              i = 3
+               i = 3
+               var div = document.createElement("div");
+               div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+               two.appendChild(div);
+
+              i = 4
               var div = document.createElement("div");
-              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-              two.appendChild(div);
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              four.appendChild(div);
 
-             i = 4
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            four.appendChild(div);
+              i = 5
+              var div = document.createElement("div");
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              five.appendChild(div);
 
-            i = 5
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            five.appendChild(div);
+              i = 6
+              var div = document.createElement("div");
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              six.appendChild(div);
 
-            i = 6
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            six.appendChild(div);
+              i = 7
+              var div = document.createElement("div");
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              seven.appendChild(div);
 
-            i = 7
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            seven.appendChild(div);
+              i = 8
+              var div = document.createElement("div");
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              eight.appendChild(div);
 
-            i = 8
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            eight.appendChild(div);
+              i = 9
+              var div = document.createElement("div");
+              div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+              nine.appendChild(div);
 
-            i = 9
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            nine.appendChild(div);
-
-            i = 10
-            var div = document.createElement("div");
-            div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType;
-            ten.appendChild(div);
+             i = 10
+             var div = document.createElement("div");
+             div.innerHTML = 'JobTitle: ' + data[i].JobTitle + "<br>" + 'Location: ' + data[i].Location + "<br>" + 'Company: ' + data[i].company + "<br>" + 'JobType: ' + data[i].JobType + "<br>" + 'category: ' + data[i].category;
+             ten.appendChild(div);
         }
        
     })
